189a1be010fcd50b27ef03391b2ee30035e956c5
Matiss Judins <matissj1337@gmail.com>
2024-09-29 18:42:37 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-gray-100 flex justify-center items-center h-[100vh]">
+        <div className="flex flex-col items-center justify-center p-4 rounded-md bg-white border border-black">
+            <span>GrapQL</span>
+        </div>
+    </div>
+  )
+}
+
+export default App
